7b1a0b79d0bf813098f16decaa565f3a987497ef
unknown <lucassodrem@gmail.com>
2019-05-22 15:24:35 -0300

    Alteracoes no server.js.

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 // Start the app by listening on the default Heroku port
